17cda3e76a2f5316041da3c41bc579eb205d7e0d
scatowl <scatowl@qq.com>
2021-04-14 14:28:21 +0800

    change the old function

--- scripts/pterfunc.py
+++ scripts/pterfunc.py
@@ -9,32 +9,84 @@ from rekt import rektlvl, rektlvls, rekt_fast
 import awsmfunc as awf
 import mvsfunc as mvf
 
-
-def solarcurve(input,css='420'):
-    core = vs.get_core()
-    clip = input
-
-    clip = mvf.ToRGB(clip,depth=8)
-    pi = math.pi
+def solarcurve(clip,color='24'):
+    '''Add a solar curve filter to clip, default is 24 colorspace.From https://github.com/jack2game/solarcurve'''
     t = 5
     k = 5.5
-    A = -1/4194304*(k*pi - 128/t)
-    B = 3/32768*(k*pi - 128/t)
-    C = 1/t
-    def curveR(x):
-        a=round(127.9999*math.sin(A*(x)**3 + B*(x)**2 + C**(x) ) + 127.5)
-        return a
-    def curveG(x):
-        a=round(127.9999*math.sin(A*(x-5)**3 + B*(x-5)**2 + C**(x-5)) + 127.5)
-        return a
-    def curveB(x):
-        a=round(127.9999*math.sin(A*(x+5)**3 + B*(x+5)**2 + C**(x+5)) + 127.5)
-        return a
-    clip = core.std.Lut(clip=clip, planes=[0], function=curveR)
-    clip = core.std.Lut(clip=clip, planes=[1], function=curveG)
-    clip = core.std.Lut(clip=clip, planes=[2], function=curveB)
-    clip = mvf.ToYUV(clip,depth=8,css=css)
-    return clip
+    m = (k * math.pi - 128 / t)
+    A = -1 / 4194304 * m
+    B = 3 / 32768 * m
+    C = 1 / t
+    def solar(clip):
+        if clip.format is None:
+            raise vs.Error("Tweak: only clips with constant format are accepted.")
+
+        if clip.format.color_family == vs.RGB:
+            raise vs.Error("Tweak: RGB clips are not accepted.")
+        
+        output = core.resize.Spline36(clip=clip, format=vs.RGB24, matrix_in_s="709")
+        output = core.std.Lut(clip=output, planes=[0], function=solar24r)
+        output = core.std.Lut(clip=output, planes=[1], function=solar24g)
+        output = core.std.Lut(clip=output, planes=[2], function=solar24b)
+        return core.resize.Spline36(clip=output, format=clip.format, matrix_s="709")
+
+    def solar48(clip):
+        if clip.format is None:
+            raise vs.Error("Tweak: only clips with constant format are accepted.")
+
+        if clip.format.color_family == vs.RGB:
+            raise vs.Error("Tweak: RGB clips are not accepted.")
+        
+        output = core.resize.Spline36(clip=clip, format=vs.RGB48, matrix_in_s="709")
+        output = core.std.Lut(clip=output, planes=[0], function=solar48r)
+        output = core.std.Lut(clip=output, planes=[1], function=solar48g)
+        output = core.std.Lut(clip=output, planes=[2], function=solar48b)
+        return core.resize.Spline36(clip=output, format=clip.format, matrix_s="709")
+    def solar24r(x):
+       return round(127.9999 * math.sin(A * (x) ** 3 + B * (x) ** 2 + C * (x) - math.pi / 2) + 127.5)
+    def solar24g(x):
+       return round(127.9999 * math.sin(A * (x-5) ** 3 + B * (x-5) ** 2 + C * (x-5) - math.pi / 2) + 127.5)
+    def solar24b(x):
+       return round(127.9999 * math.sin(A * (x+5) ** 3 + B * (x+5) ** 2 + C * (x+5) - math.pi / 2) + 127.5)
+    def solar48r(x):
+       return round((127.9999 * math.sin(A * (x/65535*255) ** 3 + B * ((x/65535*255)) ** 2 + C * ((x/65535*255)) - math.pi / 2) + 127.5)**2)
+    def solar48g(x):
+       return round((127.9999 * math.sin(A * ((x/65535*255)-5) ** 3 + B * ((x/65535*255)-5) ** 2 + C * ((x/65535*255)-5) - math.pi / 2) + 127.5)**2)
+    def solar48b(x):
+       return round((127.9999 * math.sin(A * ((x/65535*255)+5) ** 3 + B * ((x/65535*255)+5) ** 2 + C * ((x/65535*255)+5) - math.pi / 2) + 127.5)**2)
+    if color == '24':
+        return solar(clip)
+    elif color == '48':
+        return solar48(clip)
+    else:
+        raise vs.Error('Only accepts 24 or 48 as parameter')
+
+# def solarcurve(input,css='420'):
+# '''Whalehu's solarcurve'''
+#     core = vs.get_core()
+#     clip = input
+
+#     clip = mvf.ToRGB(clip,depth=8)
+#     pi = math.pi
+#     t = 5
+#     k = 5.5
+#     A = -1/4194304*(k*pi - 128/t)
+#     B = 3/32768*(k*pi - 128/t)
+#     C = 1/t
+#     def curveR(x):
+#         a=round(127.9999*math.sin(A*(x)**3 + B*(x)**2 + C**(x) ) + 127.5)
+#         return a
+#     def curveG(x):
+#         a=round(127.9999*math.sin(A*(x-5)**3 + B*(x-5)**2 + C**(x-5)) + 127.5)
+#         return a
+#     def curveB(x):
+#         a=round(127.9999*math.sin(A*(x+5)**3 + B*(x+5)**2 + C**(x+5)) + 127.5)
+#         return a
+#     clip = core.std.Lut(clip=clip, planes=[0], function=curveR)
+#     clip = core.std.Lut(clip=clip, planes=[1], function=curveG)
+#     clip = core.std.Lut(clip=clip, planes=[2], function=curveB)
+#     clip = mvf.ToYUV(clip,depth=8,css=css)
+#     return clip
 
 
 
