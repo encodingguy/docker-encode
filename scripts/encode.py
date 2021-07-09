bd06f0b1e9b0f7850bfd6e46325ac3928420c2c2
scatowl <scatowl@qq.com>
2021-07-09 20:00:54 +0800

    remove redundant code

--- scripts/encode.py
+++ scripts/encode.py
@@ -73,8 +73,7 @@ else:
 # 3 Sample Extract & Comparison
 if sample_extract:
     filtered = awf.FrameInfo(filtered, 'Filtered') if sample_comparison else filtered # Tag original frameinfo if compare sample
-    extract = awf.SelectRangeEvery(clip=awf.FrameInfo(filtered, 'Filtered') if sample_comparison else filtered, every=3000, length=50,
-                                   offset=960)  # Modify it with the length to extract!
+    extract = awf.SelectRangeEvery(filtered, every=3000, length=50,offset=960)  # Modify it with the length to extract!
     if sample_comparison:
         comparison = ptf.InterleaveDir(folder=test_folder, PrintInfo=True, first=extract, repeat=True)
         depth(comparison, 8).set_output()
